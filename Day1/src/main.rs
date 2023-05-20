b09f12ab950e9f90eb077c2841b7f99b9bcd448e
Nigel Morse <nigel.morse@altium.com>
2023-05-20 23:28:35 +0100

    Day 1 part 1

--- Day1/src/main.rs
+++ Day1/src/main.rs
@@ -1,15 +1,33 @@
 use std::env;
 use std::fs;
+use std::cmp;
 
 fn main() {
 
     let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
+
+    let file_path;
+    if args.len() < 2 {
+        file_path = r"C:\dev\AdventOfCode2022\Day1\input.txt"
+    } else {    
+        file_path = &args[1];
+    }
 
     println!("Using file {}", file_path);
 
     let contents = fs::read_to_string(file_path)
          .expect("Should have been able to read the file");
 
-    println!("With text:\n{contents}");
+    let mut acc = 0;
+    let mut highest = 0;
+    for line in contents.lines() {
+        match line.parse::<i32>() {
+            Ok(i) => acc += i,
+            Err(_) => {
+                highest = cmp::max(highest, acc);
+                acc = 0;
+            }
+        }
+    }
+    println!("{}", highest  )
 }
