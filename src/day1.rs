cf0ed6be85fbbf8c1031af79869694265fbc03de
Nigel Morse <nigel.morse@altium.com>
2023-05-21 01:33:13 +0100

    day 1 part b

--- src/day1.rs
+++ src/day1.rs
@@ -1,25 +1,23 @@
-use std::fs;
-use std::cmp;
 
 pub fn day1() {
 
-    let file_path = r"C:\dev\AdventOfCode2022\src\input.txt";
-
-    println!("Using file {}", file_path);
-
-    let contents = fs::read_to_string(file_path)
-         .expect("Should have been able to read the file");
+    let input = include_str!("day1_input.txt");
 
     let mut acc = 0;
-    let mut highest = 0;
-    for line in contents.lines() {
+    let mut elf = Vec::new();
+    for line in input.lines() {
         match line.parse::<i32>() {
             Ok(i) => acc += i,
             Err(_) => {
-                highest = cmp::max(highest, acc);
+                elf.push(acc);
                 acc = 0;
             }
         }
-    }
-    println!("{}", highest  )
+    }   
+    elf.sort();
+    elf.reverse();
+
+    println!("{}", elf[0] );
+    println!("{}", elf.into_iter().take(3).sum::<i32>() );
+
 }
