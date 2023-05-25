c4f07e03fad279b3c3537cf309db4a2aa214fa74
Nigel Morse <musmuris@users.noreply.github.com>
2023-05-25 23:00:58 +0100

    day 2 part 2 hack

--- src/day2.rs
+++ src/day2.rs
@@ -25,5 +25,26 @@ pub fn day2() {
         }
     }
 
-    println!("{}", total)
+    println!("{}", total);
+
+    let scores2 = HashMap::from([
+        ("A X", 3 + 0),
+        ("A Y", 1 + 3),
+        ("A Z", 2 + 6),
+        ("B X", 1 + 0),
+        ("B Y", 2 + 3),
+        ("B Z", 3 + 6),
+        ("C X", 2 + 0),
+        ("C Y", 3 + 3),
+        ("C Z", 1 + 6),
+    ]);
+
+    total = 0;
+    for line in input.lines() {
+        match scores2.get(line) {
+            Some(&score) => total += score,
+            _ => () 
+        }
+    }
+    println!("{}", total);
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ pub mod day1;
 pub mod day2;
 
 fn main() {
+    println!("Day 1");
     day1::day1();
+    println!("Day 2");
     day2::day2();
 }
