01aeb8c97a143ae0c616e5c209b180f2b27d7d2f
Nigel Morse <nigel.morse@altium.com>
2023-08-16 21:20:52 +0100

    Finally got a day 3b done

--- src/day3.rs
+++ src/day3.rs
@@ -1,5 +1,6 @@
 
 use std::collections::HashSet;
+use itertools::Itertools;
 
 pub fn day3() {
 
@@ -7,20 +8,46 @@ pub fn day3() {
 
     let mut acc : u32= 0;
     for line in input.lines() {
+
+        if !line.is_ascii() {
+            panic!("ASCII only string expected")
+        }
         let (first,second) = line.split_at(line.len()/2);
     
         let chars1 : HashSet<u8> = first.bytes().collect();
         let chars2 : HashSet<u8> = second.bytes().collect();
-        let same = *(chars1.intersection(&chars2).next().unwrap());
+        let sames = chars1.intersection(&chars2);
+        let same = sames.at_most_one();
 
-        acc = acc + match same {
-            b'a'..=b'z' => same - b'a' + 1,
-            b'A'..=b'Z' => same - b'A' + 27,
-            _ => panic!("something wonderful has happened")
-        } as u32;
-        
-        println!("{}", acc);
+        acc = acc + map_priority(*same.unwrap().unwrap())        
     }
 
     println!("{}", acc);
+    acc = 0;
+    for set in &input.lines().chunks(3) {
+        let chunk_lines : Vec<HashSet<u8>> = set.map(|x| x.bytes().collect()).collect();
+        let mut found: Option<u8> = None;
+        for c in &chunk_lines[0] {
+            if chunk_lines[1].contains(c) && chunk_lines[2].contains(c) {
+                found = match found {
+                    None => Some(*c),
+                    Some(_) => panic!("more than one found")
+                }
+            }
+        }
+        if found == None {
+            panic!("No match found")
+        }
+
+        acc = acc + map_priority(found.unwrap())        
+    }
+    println!("{}", acc);
+}
+
+fn map_priority(item: u8) -> u32 {
+    match item {
+        b'a'..=b'z' => item - b'a' + 1,
+        b'A'..=b'Z' => item - b'A' + 27,
+        _ => panic!("Expected a..z or A..Z characters only")
+    }.into()
 }
