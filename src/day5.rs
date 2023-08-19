7b837fa5fc8c6bcc6809979789600a7996e81bb4
Nigel Morse <musmuris@users.noreply.github.com>
2023-08-19 21:43:35 +0100

    Day 5 rest of part 1 and 2
    But leaves me with some questions on rust

--- src/day5.rs
+++ src/day5.rs
@@ -3,14 +3,14 @@ pub fn day5() {
 
     let (stack_lines, move_lines) = input.split_once("\r\n\r\n").unwrap();
 
-    let mut s = stack_lines.lines().rev();
-    let first = s.next().unwrap();
+    let mut stack_iterator = stack_lines.lines().rev();
+    let first = stack_iterator.next().unwrap();
 
     let last_col = first.split_ascii_whitespace().last().unwrap();
     let stack_count = last_col.parse().unwrap();
     let mut stacks = vec![vec![]; stack_count];
 
-    for line in s {
+    for line in stack_iterator {
         for (index, character) in line.chars().enumerate().skip(1).step_by(4) {
             if character.is_ascii_alphabetic() {
                 let stack_num = (index - 1) / 4;
@@ -19,6 +19,40 @@ pub fn day5() {
         }
     }
 
-    println!("{:?}", stacks);
-    
+    let mut stacks2 = stacks.clone();
+    for line in move_lines.lines() {
+        let mut s = line.split_ascii_whitespace();
+        let count: usize = s.by_ref().skip(1).next().unwrap().parse().unwrap();
+        let from: usize = s.by_ref().skip(1).next().unwrap().parse().unwrap();
+        let to: usize = s.skip(1).next().unwrap().parse().unwrap();
+        for _ in 0..count {
+            let p = stacks[from - 1].pop().unwrap();
+            stacks[to - 1].push(p)
+        }
+
+        // Why can't I  (works if you get len then split_off on two lines)
+        // But I guess here the imutable borrow happens after the mutable one
+        // let end = stacks2[from-1].split_off(stacks2[from-1].len() - count);
+        // stacks2[to-1].extend(end);
+
+        // Or
+        //let end = stacks2[from-1].iter().rev().take(3);
+        //stacks2[to-1].extend(end);
+
+        let n = stacks2[from - 1].len() - count;
+        let end = stacks2[from - 1].split_off(n);
+        stacks2[to - 1].extend(end);
+    }
+
+    println!("{}", make_result(&stacks));
+    println!("{}", make_result(&stacks2));
+}
+
+
+fn make_result(stacks: &Vec<Vec<char>>) -> String {
+    let mut result = String::new();
+    for stack in stacks.iter() {
+        result.push(stack[stack.len() - 1]);
+    }
+    return result;
 }
