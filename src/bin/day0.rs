0dedc2f9c776182d4485966a832dfd6e1a6b369f
Nigel Morse <musmuris@users.noreply.github.com>
2023-08-22 19:38:26 +0100

    Add a day 0 as a template

---
+++ src/bin/day0.rs
@@ -0,0 +1,26 @@
+
+pub fn day0(input: &str) -> (usize, usize) {
+
+    return (input.len(), input.len());
+}
+
+fn main() {
+    let input = include_str!("day0.txt");
+    let (p1,p2) = day0(input);
+    println!("{}\n{}", p1, p2);
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1()
+    {
+        let (p1,p2) = day0("1234");
+        
+        assert_eq!(p1, 4);
+        assert_eq!(p2, 4);        
+    }
+}
