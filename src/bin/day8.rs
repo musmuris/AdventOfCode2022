dffa9f606a28c3c1cef1ba1a4f936d697eb2fe5b
Nigel Morse <musmuris@users.noreply.github.com>
2023-09-09 09:22:54 +0100

    Redo V8 part1 so part 2 can be done at same time

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -1,44 +1,72 @@
+use std::u8;
+
+struct TreeChecker<'a> {
+    trees: &'a Vec<&'a [u8]>,
+}
+
+impl<'a> TreeChecker<'a> {
+    fn new(trees: &'a Vec<&'a [u8]>) -> TreeChecker<'a> {
+        TreeChecker { trees }
+    }
+
+    fn check_tree_in_direction(
+        &mut self,
+        row: usize,
+        col: usize,
+        delta_row: isize,
+        delta_col: isize,
+    ) -> (bool, u32) {
+        let mut visible = true;
+        let mut score = 0;
+
+        let current = self.trees[row][col];
+        let mut row = (row as isize) + delta_row;
+        let mut col = (col as isize) + delta_col;
+
+        while let Some(&height) = self
+            .trees
+            .get(row as usize)
+            .and_then(|x| x.get(col as usize))
+        {
+            score += 1;
+            if current <= height {
+                visible = false;
+                break;
+            }
+            row += delta_row;
+            col += delta_col;
+        }
+
+        (visible, score)
+    }
+}
+
 pub fn day8(input: &str) -> (u32, u32) {
     let trees: Vec<&[u8]> = input.lines().map(|l| l.as_bytes()).collect();
 
-    let row_len = trees[0].len();
-    let mut number = 0;
-    let mut visible: Vec<bool> = vec![false; trees.len() * row_len];
-    let mut max: u8;
+    let mut tc = TreeChecker::new(&trees);
 
-    let mut check_tree = |row: usize, col: usize, max2: &mut u8| {
-        if trees[row][col] > *max2 {
-            *max2 = trees[row][col];
-            if !visible[row * row_len + col] {
-                visible[row * row_len + col] = true;
-                number += 1;
+    let (mut vis_count, mut max_score) = (0, 0);
+    for row in 0..trees.len() {
+        for col in 0..trees[row].len() {
+            let (mut visible, mut score) = (false, 1);
+
+            for (dr, dc) in [(-1, 0), (0, -1), (1, 0), (0, 1)] {
+                let (vis, sc) = tc.check_tree_in_direction(row, col, dr, dc);
+                visible |= vis;
+                score *= sc;
+            }
+
+            if visible {
+                vis_count += 1;
+            }
+            if score > max_score {
+                max_score = score;
             }
         }
-    };
-
-    for row in 0..trees.len() {
-        max = 0;
-        for col in 0..row_len {
-            check_tree(row, col, &mut max);
-        }
-        max = 0;
-        for col in (0..row_len).rev() {
-            check_tree(row, col, &mut max);
-        }
     }
 
-    for col in 0..row_len {
-        max = 0;
-        for row in 0..trees.len() {
-            check_tree(row, col, &mut max);
-        }
-        max = 0;
-        for row in (0..trees.len()).rev() {
-            check_tree(row, col, &mut max);
-        }
-    }
-
-    return (number, 0);
+    return (vis_count, max_score);
 }
 
 fn main() {
@@ -53,15 +81,16 @@ mod tests {
 
     #[test]
     fn test1() {
-        let (p1,p2) = day8("30373
+        let (p1, p2) = day8(
+            "30373
 25512
 65332
 33549
 35390
-");
-        
+",
+        );
 
         assert_eq!(p1, 21);
-        //assert_eq!(p2, 21);
+        assert_eq!(p2, 8);
     }
 }
