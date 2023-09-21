077a5ba889714f0d303853e89a223c4143f08ce9
Nigel Morse <musmuris@users.noreply.github.com>
2023-09-22 00:24:16 +0100

    For P2 invert the walk and allow different "start"

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -5,17 +5,17 @@ const INPUT: &str = include_str!("day12.txt");
 #[derive(PartialEq, PartialOrd, Eq, Ord, Clone, Copy)]
 struct Pos(usize, usize);
 
-fn grid_walk(grid: &[&[u8]], start: Pos) -> u32 {
+fn grid_walk(grid: &[&[u8]], end: Pos, start_height: u8) -> u32 {
     let mut queue: VecDeque<(Pos, u8)> = VecDeque::new();
     let mut distance: BTreeMap<Pos, u32> = BTreeMap::new();
 
-    distance.insert(start, 0);
-    queue.push_back((start, b'a'));
+    distance.insert(end, 0);
+    queue.push_back((end, b'z'));
 
     while !queue.is_empty() {
         let (pos, current_height) = queue.pop_front().unwrap();
 
-        if current_height == b'E' {
+        if current_height == start_height {
             return distance[&pos];
         }
 
@@ -26,8 +26,8 @@ fn grid_walk(grid: &[&[u8]], start: Pos) -> u32 {
             );
             if !distance.contains_key(&neighbour) {
                 if let Some(&height) = grid.get(neighbour.0).and_then(|l| l.get(neighbour.1)) {
-                    let real_height = if height == b'E' { b'z' } else { height };
-                    if real_height <= current_height + 1 {
+                    let real_height = if height == b'S' { b'a' } else { height };
+                    if real_height >= current_height - 1 {
                         queue.push_back((neighbour, height));
                         distance.insert(neighbour, distance[&pos] + 1);
                     }
@@ -38,33 +38,24 @@ fn grid_walk(grid: &[&[u8]], start: Pos) -> u32 {
     panic!("No path")
 }
 
-pub fn day12(input: &str) -> (u32, usize) {
-    let mut start = Pos(0, 0);
+pub fn day12(input: &str) -> (u32, u32) {
+    let mut end = Pos(0, 0);
     let grid: Vec<&[u8]> = input
         .lines()
         .enumerate()
         .map(|(line_inx, line_str)| {
             let line = line_str.as_bytes();
-            if let Some(i) = line.iter().position(|&b| b == b'S') {
-                start = Pos(line_inx, i)
+            if let Some(i) = line.iter().position(|&b| b == b'E') {
+                end = Pos(line_inx, i)
             }            
             line
         })
         .collect();
 
-    let p1 = grid_walk(&grid, start);
-
-    let mut distance: BTreeMap<&str, u32> = BTreeMap::new();
-   
-    distance.insert("near", 10);
-   
-    if !distance.contains_key("far") {
-        distance.insert("far", distance["near"] + 1);
-    }
+    let p1 = grid_walk(&grid, end, b'S');
+    let p2 = grid_walk(&grid, end, b'a');
     
-    dbg!(distance);
-
-    (p1, input.len())
+    (p1, p2)
 }
 
 fn main() {
@@ -82,7 +73,7 @@ mod tests {
         let (p1, p2) = day12(input);
 
         assert_eq!(p1, 31);
-        assert_eq!(p2, 50);
+        assert_eq!(p2, 29);
     }
 
     #[test]
@@ -90,6 +81,6 @@ mod tests {
         let (p1, p2) = day12(INPUT);
 
         assert_eq!(p1, 520);
-        assert_eq!(p2, 7093);
+        assert_eq!(p2, 508);
     }
 }
